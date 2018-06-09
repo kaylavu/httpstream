a0519deb3d8bf000830b53c9aa00798f96019dff
kaylavu <kaylaavu@gmail.com>
2018-06-09 23:58:04 +0000

    step 3

--- https_example.js
+++ https_example.js
@@ -3,3 +3,18 @@ var x = "hello"
 var https = require('https');
 
 console.log('I did it!');
+
+var options = { 
+    host: 'www.example.org', 
+    path: '/': 
+}; 
+
+//called by https when the request is made
+var callback = funtion() { 
+    console.log('In response handler callback!');
+}
+
+console.log("I'm about to make the request!");
+
+https.request(options,callback).end(); 
+console.log("I've made the request!");
