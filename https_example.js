e7a7cccebf614b8acc37461a28805aba5f4eb846
kaylavu <kaylaavu@gmail.com>
2018-06-10 00:51:14 +0000

    step6

--- https_example.js
+++ https_example.js
@@ -6,15 +6,23 @@ console.log('I did it!');
 
 var options = { 
     host: 'www.example.org', 
-    path: '/': 
-}; 
+    path: '/',
+}
 
 //called by https when the request is made
-var callback = funtion() { 
+var callback = function(response) { 
     console.log('In response handler callback!');
+    
+    response.on('data', function(chunk) {
+        console.log('[-- CHUNK OF LENGTH ' + chunk.length + ' --]');
+        console.log(chunk.toString());
+      });
 }
 
 console.log("I'm about to make the request!");
 
-https.request(options,callback).end(); 
+https.request(options,callback).end();
+
 console.log("I've made the request!");
+
+
